801e9d61d1aef65ca67235676bb01223553b32d2
TBSergio <TBSergio@walla.com>
2018-03-11 22:14:52 +0200

    Added yargs parsing

--- node-notes/app.js
+++ node-notes/app.js
@@ -1,8 +1,29 @@
 // 3rd party dependencies //
 const fs = require('fs');
 const _ = require('lodash');
+const yarg = require('yargs');
 
 // local dependencies //
 const note = require('./note.js')
 
 // main //
+const argv = yarg.argv;
+var command = process.argv[2];
+
+
+if(command === 'add'){
+    note.addNote(argv.title,argv.body);
+}
+else if (command === 'list'){
+    note.getAll();
+}
+else if (command === 'remove'){
+    note.removeNote(argv.title);
+}
+else if (command === 'read'){
+    note.readNote(argv.title);
+}
+else{
+    console.log('Command not found');
+}
+
--- node-notes/note.js
+++ node-notes/note.js
@@ -3,6 +3,22 @@
 // local dependencies //
 
 // main //
-module.exports.addNote = () => {
-    console.log("Adding note");
+var addNote = (title, body) => {
+    console.log('Adding note:',title,body);
+};
+var getAll = () => {
+    console.log('List of all Notes:');
+};
+var removeNote = (title) => {
+    console.log('Removing note titled:',title);
+};
+var readNote = (title) => {
+    console.log('Note',title,'Contains: ');
+};
+
+module.exports = {
+    addNote: addNote,
+    getAll: getAll,
+    readNote,           //in ES6 you are not requiered to specify the type if it is exactly the same as the function name
+    removeNote,         //equivelent to the first 2 lines
 };
