8c6a6a02bbc225595934ef45a3e5948cea7fd9f0
TBSergio <TBSergio@walla.com>
2018-03-11 22:29:44 +0200

    Saving and reading data from a json file format.

--- node-notes/playground/testingJSON.js
+++ node-notes/playground/testingJSON.js
@@ -1,5 +1,25 @@
-var obj = {
-    name: 'Sergei'
+// var JSONobj = {
+//     name: 'Sergei',
+//     age: 22
+// };
+// var stringObj =  JSON.stringinfy(JSONobj);
+
+// var personString = '{"name":"Sergei","age":22}';
+// var person = JSON.parse(personString);
+
+//3rd party dependencies
+const fs = require('fs');
+
+//main
+var originalNote = {
+    title: 'Some Title',
+    body: 'Some Body'
 };
 
-var stringObj =  JSON.stringinfy(obj);
+fs.writeFileSync('notes.json',JSON.stringify(originalNote));
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
