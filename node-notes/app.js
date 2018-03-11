14e4bc1c3b430494c739c6208d29adc6937efc8d
TBSergio <TBSergio@walla.com>
2018-03-11 20:50:13 +0200

    changed the hierarchy of the project to sit under a sub folder of the whole course since there will be multiple projects in the course

--- node-notes/app.js
+++ node-notes/app.js
@@ -1,4 +1,4 @@
-console.log("Starting app.js");
+
 const fs = require('fs');
 const note = require('./note.js')
 const _ = require('lodash');
@@ -11,5 +11,3 @@ fs.appendFile('greetings.txt',`Hello ${note.userName}!\n`,(err)=>{
 console.log("isString Returns: ",_.isString("String"));
 var filteredArray = _.uniq(['Sergei',1,2,3,3,'Sergei']);
 console.log("filteredArray is: ",filteredArray);
-
-console.log("Finished app.js");
